16cf72c249a7c44349643d47d7865abad34cb212
Madhava1104 <madhavagmail.com>
2024-12-19 16:34:02 +0530

    Added Press 4 and Removed Humidity Ramp Rate

--- climate_controller.py
+++ climate_controller.py
@@ -53,19 +53,17 @@ class TemperatureHumidityController:
                 time.sleep(2)
         return current_temperature
     
-    def adjust_humidity(self, current_humidity, new_humidity, humidity_ramp_rate):
+    def adjust_humidity(self, current_humidity, new_humidity):
         """
         Gradually adjust the humidity to the desired setpoint.
 
         Args:
             current_humidity (float): The current humidity value.
             new_humidity (float): The target humidity setpoint.
-            humidity_ramp_rate (float): The rate at which the humidity changes (percentage per minute).
 
         Steps:
             - Increments or decrements the humidity gradually.
             - Updates the humidity in the Modbus register after every step.
-            - Waits for 60 seconds between steps to simulate real-world ramping.
 
         Note:
             If the current humidity equals the new humidity, no adjustment is made.
@@ -73,11 +71,8 @@ class TemperatureHumidityController:
         if current_humidity != new_humidity:
             logger.info(f"Adjusting humidity from {current_humidity} to {new_humidity}...")
             while current_humidity != new_humidity:
-                time.sleep(60)
-                step = humidity_ramp_rate if current_humidity < new_humidity else -humidity_ramp_rate
-                current_humidity += step
-                current_humiditys = max(min(new_humidity, current_humidity), min(current_humidity, new_humidity))
-                self.client.write_register(self.humidity_register, int(current_humiditys))
+                current_humidity = new_humidity
+                self.client.write_register(self.humidity_register, int(current_humidity))
                 logger.info(f"Current Humidity: {current_humidity:.2f}")
-                time.sleep(2)
+                time.sleep(1)
         return current_humidity
